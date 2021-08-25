8547c53ba69b15d465ced74e2facc5bcf707006d
Divyabharathi <87221593+DivyabharathiManimaran@users.noreply.github.com>
2021-08-25 22:23:38 +0530

    Update home-component.service.ts

--- src/app/service/home-component.service.ts
+++ src/app/service/home-component.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient} from "@angular/common/http";
 import { PlayerDetails, PlayerInfo } from "../models/player";
-import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
