876181041e54939eafc791d278aa7983f17fcb2f
DivyabharathiManimaran <divyamanimaran96@gmail.com>
2021-08-25 22:45:48 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home-component/home-component';
 
 const routes: Routes = [  
-  { path:'player-archive', component:HomeComponent },
-  { path: '', redirectTo:'/player-archive', pathMatch: 'full'},
+  { path:'', component:HomeComponent },
+  // { path: '', redirectTo:'/player-archive', pathMatch: 'full'},
 ];
 
 @NgModule({
