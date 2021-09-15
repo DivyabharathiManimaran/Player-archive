336ef7f2a50a02dcfa04f6270007f861d8af7c9b
DivyabharathiManimaran <divyamanimaran96@gmail.com>
2021-09-15 08:56:28 +0530

    json conversion and feedback updation

--- src/app/service/home-component.service.ts
+++ src/app/service/home-component.service.ts
@@ -10,10 +10,10 @@ export class HomeComponentService {
     constructor( private readonly http: HttpClient) {}
 
     getPlayerData(name: string) {
-        return this.http.get<PlayerInfo>(`https://web-sandbox.onefootball.com/assignments/player/data/${name}.json`);
+        return this.http.get<PlayerInfo>(`assets/jsons/player-data/${name}.json`);
     }
     getPlayerProfile(profileId: string) {
-        return this.http.get<PlayerDetails>(`https://web-sandbox.onefootball.com/assignments/player/profile/${profileId}`);
+        return this.http.get<PlayerDetails>(`assets/jsons/player-profile/${profileId}`);
     }
 
 
